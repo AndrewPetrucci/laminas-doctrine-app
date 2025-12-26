27ef755d80c3bb96d22e4ca262598d095d6420b2
Andrew Petrucci <andrewpetrucci@gmail.com>
2025-12-25 21:52:24 -0800

    feat: ingest poe json

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -3,14 +3,52 @@
 declare(strict_types=1);
 
 namespace Application\Controller;
-
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
-    public function indexAction()
+    public function skilltreeAction()
     {
-        return new ViewModel();
+        $jsonFile = __DIR__ . '/../../../data/skilltree.json';
+        $jsonData = json_decode(file_get_contents($jsonFile), true);
+
+        $nodes = [];
+        $edges = [];
+
+        // Build nodes
+        foreach ($jsonData['nodes'] as $nodeId => $node) {
+            $nodes[] = [
+                'data' => [
+                    'id' => $nodeId,
+                    'label' => $node['dn'] ?? 'Unnamed'
+                ],
+                'position' => [
+                    'x' => $node['x'] ?? 0,
+                    'y' => $node['y'] ?? 0
+                ]
+            ];
+
+            // Build edges for each outgoing connection
+            if (!empty($node['out'])) {
+                foreach ($node['out'] as $targetId) {
+                    $edges[] = [
+                        'data' => [
+                            'source' => $nodeId,
+                            'target' => $targetId
+                        ]
+                    ];
+                }
+            }
+        }
+
+        $view = new ViewModel([
+            'cyNodes' => json_encode($nodes),
+            'cyEdges' => json_encode($edges)
+        ]);
+        
+        $view->setTerminal(true); // disables layout
+        return $view;
     }
 }
+
